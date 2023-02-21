663ea957561d6605dc792149852cffda53860d4c
KaterynaChernetska <116261804+KaterynaChernetska@users.noreply.github.com>
2023-02-21 19:52:12 +0000

    Deploying to gh-pages from @ KaterynaChernetska/goit-react-hw-05-movies@02d36ff197c001e2f6921638bedeb2c086d62ac1 🚀

---
+++ static/js/459.e358abbb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[459],{459:function(e,t,n){n.r(t);var o=n(87),i=n(184);t.default=function(){return(0,i.jsxs)("div",{children:["Page not found. Go ",(0,i.jsx)(o.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=459.e358abbb.chunk.js.map
